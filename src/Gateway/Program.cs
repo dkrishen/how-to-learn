a7402bdd086eeb84e4569c5417dddccd0674b475
dkrishen <d.krishen@softteco.com>
2023-10-03 15:13:48 +0300

    the gateway project was cleaned of template content

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,8 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
