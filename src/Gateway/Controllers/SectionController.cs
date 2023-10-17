6ca956dfd7bad63596075097326362716ba40f0c
dkrishen <d.krishen@softteco.com>
2023-10-17 12:21:45 +0300

    section controller was created

---
+++ src/Gateway/Controllers/SectionController.cs
@@ -0,0 +1,54 @@
+﻿using Gateway.Logic;
+using Gateway.Models.PostDto;
+using Gateway.Models.Update;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectionController : ControllerBase
+    {
+        private readonly ISectionLogic _sectionLogic;
+
+        public SectionController(ISectionLogic sectionLogic)
+        {
+            _sectionLogic = sectionLogic;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var result = await _sectionLogic.GetSectionAsync(id).ConfigureAwait(false);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _sectionLogic.GetSectionsAsync().ConfigureAwait(false);
+            return Ok(result);
+        }
+
+        [HttpPost]  
+        public async Task<IActionResult> Post([FromBody] SectionPostDto section)
+        {
+            await _sectionLogic.AddSectionAsync(section).ConfigureAwait(false);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] Guid id)
+        {
+            await _sectionLogic.DeleteSectionAsync(id).ConfigureAwait(false);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] SectionUpdateDto section)
+        {
+            await _sectionLogic.UpdateSectionAsync(section).ConfigureAwait(false);
+            return Ok();
+        }
+    }
+}
