d5f0fea33a8fdacb7337475cf881212ca8176bba
dkrishen <dkrishen@yandex.by>
2024-02-20 12:42:04 +0300

    missed changes for previous commit

---
+++ src/Gateway/Models/Elastic/SectionResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using Gateway.Models.View;
+using System.Text.Json.Serialization;
+
+namespace Gateway.Models.Elastic;
+
+public class SectionResponseDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = null!;
+
+    [JsonPropertyName("topics")]
+    public TopicViewDto[] Topics { get; set; }
+}
